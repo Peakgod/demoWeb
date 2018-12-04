3588c39743c8fe3f69dcfce50dc34aa6ccd96156
Peakgod <2832312250@qq.com>
2018-12-04 14:46:54 +0800

    功能基本实现，验证欠缺

--- pages/practice/second.vue
+++ pages/practice/second.vue
@@ -3,7 +3,7 @@
     <div class="container-top">
       <span>
         My Second Demo 
-        <el-button type="info" @click="dialogFormVisible = true">添加数据</el-button>
+        <el-button type="info" @click="addInformation">添加数据</el-button>
       </span>
     </div>
 
@@ -38,17 +38,16 @@
         <el-table-column
         fixed="right"
         label="操作"
-         width="200">
+         width="140">
           <template slot-scope="scope" class="ele-btn">
-            <el-button type="success">查看</el-button>
-            <el-button type="info" @click="editorData (scope.$index, scope.row) ">编辑</el-button>
+            <el-button type="info" @click="editorInformation(scope.$index, scope.row) ">编辑</el-button>
             <el-button type="danger" @click="deleteData (scope.$index, scope.row) ">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
     </div>
 
-    <el-dialog title="新增数据" :visible.sync="dialogFormVisible">
+    <el-dialog :title="form && form._id ? '编辑数据' : '新增数据' " :visible.sync="dialogFormVisible">
       <el-form ref="andForm" :model="form">
         <el-form-item label="port" :label-width="formLabelWidth">
           <el-input v-model="form.port" autocomplete="off"></el-input>
@@ -61,18 +60,18 @@
         </el-form-item>
         <el-form-item label="startDate" :label-width="formLabelWidth">
           <el-col :span="11">
-            <el-date-picker type="date" placeholder="选择日期" v-model="form.startDate" style="width: 100%;"></el-date-picker>
+            <el-date-picker type="date" placeholder="选择日期" v-model="form.startDate"></el-date-picker>
           </el-col>
         </el-form-item>
         <el-form-item label="endDate" :label-width="formLabelWidth">
           <el-col :span="11">
-            <el-date-picker type="date" placeholder="选择日期" v-model="form.endDate" style="width: 100%;"></el-date-picker>
+            <el-date-picker type="date" placeholder="选择日期" v-model="form.endDate"></el-date-picker>
           </el-col>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="newMethods('form')">确 定</el-button>
+        <el-button type="primary" @click="form && form._id ? editorData ('form') : addMethods('form') ">确 定</el-button>
       </div>
     </el-dialog>
   </div>
@@ -105,7 +104,7 @@ export default {
   },
 
   methods: {
-    // 查询
+    // 查询的方法
     getRequest () {
       axios.get(this.path)
         .then(response => {
@@ -127,54 +126,94 @@ export default {
         })
     },
     // 判断样式的方法
-    tableRowClassName (row) {
-      let item = row.row.endDate
+    tableRowClassName (data) {
+      let item = data.row.endDate
       if (item) {
         if (new Date(item).getTime() <= new Date().getTime()) {
           return 'warning-row'
         }
       }
     },
-    // 添加数据
-    newMethods (form) {
-      this.dialogFormVisible = false
+    // 添加数据按钮
+    addInformation () {
+      this.form = {}
+      this.dialogFormVisible = true
+    },
+    // 添加数据网络请求
+    addMethods (form) {
       axios.post(this.path, this.form)
         .then(response => {
+          this.dialogFormVisible = false
           this.getRequest()
-          this.$refs.andForm.resetFields()
+          this.$message({
+            message: '恭喜您，添加成功！',
+            type: 'success'
+          })
         })
         .catch(error => {
           console.log(error)
+          this.$message({
+            message: '不好意思，添加失败！',
+            type: 'info'
+          })
         })
     },
+
     // 编辑
-    editorData (index, row) {
-      let item = Object.assign({}, row)
-      console.log(item)
-      this.$confirm('div', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消'
-      })
+    editorInformation (index, row) {
+      this.form = Object.assign({}, row)
+      this.dialogFormVisible = true
     },
-    // 删除
+
+    editorData (form) {
+      axios.put(`${this.path}/${this.form._id}`, this.form)
+        .then(response => {
+          this.dialogFormVisible = false
+          this.$message({
+            message: '恭喜您，修改成功！',
+            type: 'success'
+          })
+          this.getRequest()
+        })
+        .catch(error => {
+          console.log(error)
+          this.$message({
+            message: '不好意思，修改失败！',
+            type: 'info'
+          })
+        })
+    },
+
+    // 删除的方法
     deleteData (index, row) {
       let item = Object.assign({}, row)
-      this.$confirm('此操作将永久删除该数据, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
+      this.$confirm('此操作将永久删除该数据, 是否继续?', '警告', {
         type: 'warning'
       }).then(() => {
-        this.pageLoading = true
-        axios.delete(this.path, { data: { id: item._id } })
+        axios.delete(`${this.path}/${item._id}`)
           .then(res => {
-            console.log(res)
-            this.pageLoading = false
+            this.$message({
+              showClose: true,
+              message: '恭喜你，删除成功！',
+              type: 'success'
+            })
+            this.getRequest()
           }).catch(e => {
             console.log(e)
-            this.pageLoading = false
+            this.$message({
+              message: '抱歉，删除失败！',
+              type: 'success'
+            })
           })
+      }).catch(e => {
+        console.log(e)
+        this.$message({
+          message: '取消删除！',
+          type: 'success'
+        })
       })
     }
+
   }
 }
 </script>
@@ -214,7 +253,7 @@ export default {
     padding: 4px 5px;
     margin: 0;
 }
-@media (max-width: 1000px) {
+@media (max-width: 1100px) {
   .el-table {
     width: 80%
   }
