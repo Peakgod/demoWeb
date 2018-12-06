23c94f097a3bd1b243d5aacfb897fd5339ebdda3
Peakgod <2832312250@qq.com>
2018-12-06 17:53:39 +0800

    about页面修改

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,30 @@
 <template>
-      <div class="style">
-        <div class="container-top">
-          <div class="top-left"></div>
-          <div class="top-right"></div>
+  <div class="style">
+    <img src="~assets/images/yaso.jpg">
+    <!-- 侧边栏 -->
+    <div class="container-left">
+      <div class="container-left-top">
+        <div class="left-photo">
+           <img src="~assets/images/touxiang.jpg">
         </div>
-        <div class="container-bottom"></div>
+        <div class="left-text">
+          <ul>
+            <li>NAME：<span>冷</span></li>
+            <li>Birthday：<span>1997.09</span></li>
+            <li>Trick：<span>WEB</span></li>
+          </ul>
+        </div>
       </div>
+      <div class="container-left-bgc">
+        <div class="container-left-btn">
+          <el-button type="warning"> Movement </el-button>
+          <el-button type="warning"> Basketball </el-button>
+          <el-button type="warning"> GAME </el-button>
+        </div>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
@@ -22,10 +41,59 @@ export default {
 
 .style{
   width: @var-width;
-  height: @var-height;
-  background-color: #666;
-  .container-top{
-    width: @var-width - 20%;
+  img {
+    width: 100%;
+    border-radius: 10px;
+  }
+  .container-left{
+    width: 20%;
+    border-radius: 30px;
+    background-color: rgba(202, 202, 202, 0.35);
+    position: fixed;
+    top: 10%;
+    left: 4%;
+    bottom: 10%;
+    .container-left-top {
+      height: @var-height - 65%;
+      display: inline-flex;
+      .left-photo {
+        width:  @var-width - 50%;
+        padding: 30px;
+      }
+      .left-text {
+        width:  @var-width - 50%;
+        padding-top: 50px;
+        ul {
+          list-style: none;
+          padding: 0;
+          li {
+            padding: 5px;
+            span{
+              color: rgb(16, 134, 138);
+            }
+          }
+        }
+      }
+    }
+    .container-left-bgc {
+      height: 65%;
+      border-radius: 30px;
+      background-color: rgba(209, 209, 209, 0.5);
+      text-align: center;
+      .container-left-btn {
+        display: flex;
+        flex-flow: column;
+        align-items: center;
+        padding-top: 20px;
+      }
+      .el-button+.el-button{
+        margin: 5px;
+      }
+      .el-button--warning{
+        margin: 5px;
+        width: 50%;
+      }
+    }
   }
 }
 </style>
--- pages/practice/third.vue
+++ pages/practice/third.vue
@@ -1,13 +1,32 @@
 <template>
-        
+  <div class="content">
+    <div class="cont-left"></div>
+    <div class="cont-right"></div>
+  </div>
 </template>
     
 <script>
   export default {
+    method () {
+    }
   }
 </script>
     
 <style>
-    
+    .content{
+      width: 100%;
+      height: 800px;
+      background-color: #fff;
+    }
+    .cont-left{
+      width: 20%;
+      height: 800px;
+      background-color: #666;
+    }
+    .cont-right{
+      width: 80%;
+      height: 800px;
+      background-color: #666;
+    }
 </style>
     
